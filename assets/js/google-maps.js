e4024c80dcf03104351587dd1215733f20ba0671
Alejandro Fernández Pérez <alex.audiovisual.engineer@gmail.com>
2023-06-29 16:43:34 +0200

    22º commit

--- assets/js/google-maps.js
+++ assets/js/google-maps.js
@@ -1,6 +1,6 @@
 function initMap() {
     // Latitude and Longitude
-    var myLatLng = {lat: -3.917150474442195, lng: 40.39671973267111};
+    var myLatLng = {lat: 40.39671973267111, lng: -3.917150474442195};
 
     var map = new google.maps.Map(document.getElementById('google-maps'), {
         zoom: 17,
